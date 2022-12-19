05ea4f17502f1824d653728fb1b929bac44900d6
Aniketc72 <69510896+Aniketc72@users.noreply.github.com>
2022-12-19 07:37:41 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class break1
+{
+    static void Main(string[] args)
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        /* while loop execution */
+        while (a < 20)
+        {
+            Console.WriteLine("value of a: {0}", a);
+            a++;
+            if (a > 15)
+            {
+                /* terminate the loop using break statement */
+                break;
+            }
+        }
+        Console.ReadLine();
+    }
+}
