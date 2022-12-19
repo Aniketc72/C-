ee815478a453223f78acfa47e8bb584dca57b36d
Aniketc72 <69510896+Aniketc72@users.noreply.github.com>
2022-12-19 07:42:27 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿class sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sort a string
+            string[] college = { "Reva", "CMR", "BMS", "New Horizon" };
+            Array.Sort(college);
+            foreach (string i in college)
+            {
+                Console.WriteLine(i);
+            }
+            // Sort an int
+            int[] myNumbers = { 5, 1, 8, 9 };
+            Array.Sort(myNumbers);
+            foreach (int i in myNumbers)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
