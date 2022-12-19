04a21a59d553fcdc218143a325a5b1971a01aed0
Aniketc72 <69510896+Aniketc72@users.noreply.github.com>
2022-12-19 07:43:37 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿class boxingunboxing
+ {
+    static void Main(string[] args)
+    {
+        int i = 123;
+        object o = i;
+        int j = (int)o;
+        Console.WriteLine("The value-type value = {0}", i);
+        Console.WriteLine("The object-type value = {0}", o);
+        Console.WriteLine("The object-type changed value = {0}", j);
+        Console.ReadLine();
+    }
+}
