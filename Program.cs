03d43d7a0c49327d12b530150957fb3dab5143ed
Aniketc72 <69510896+Aniketc72@users.noreply.github.com>
2022-12-19 07:43:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        int[] array = { 2, 11, 15, 1, 7, 99, 6, 85, 4 };
+        Array.Sort(array); //sorting array 
+        Array.Reverse(array);
+        Console.WriteLine("Third Highest Value in Array " + array[2]);
+        foreach (var result in array)
+        {
+            Console.Write(result + " "); // Array values 
+        }
+    }
+}
