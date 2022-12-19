cb35d8dd53a18c8bf6dc5bfab46f6ff981462564
Aniketc72 <69510896+Aniketc72@users.noreply.github.com>
2022-12-19 07:38:17 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿class continue1
+{
+    static void Main(string[] args)
+    {
+        int a = 10;
+        /* do loop execution */
+        do
+        {
+            if (a == 15)
+            {
+                /* skip the iteration */
+                a = a + 1;
+                continue;
+            }
+            Console.WriteLine("value of a: {0}", a);
+            a++;
+        }
+        while (a < 20);
+        Console.ReadLine();
+    }
+}
