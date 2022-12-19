c8e1c1131876d1500d2e96e12709d67f076b7fd4
Aniketc72 <69510896+Aniketc72@users.noreply.github.com>
2022-12-19 07:35:48 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        int time = 22;
+        if (time < 10)
+        {
+            Console.WriteLine("Good morning");
+        }
+        else if (time < 20)
+        {
+            Console.WriteLine("Good day");
+
+        }
+        else
+        {
+            Console.WriteLine("Good Evening");
+        }
+        Console.ReadLine();
+    }
+}
