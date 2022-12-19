884a5876f944b552fd11fbb909de6c18496fc6a8
Aniketc72 <69510896+Aniketc72@users.noreply.github.com>
2022-12-19 07:40:32 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿class Reverse
+{
+    static void Main(string[] args)
+    {
+        String myStr, rev;
+        rev = "";
+        myStr = Console.ReadLine();
+        Console.WriteLine("String is {0}", myStr);
+        // find string length
+        int len;
+        len = myStr.Length - 1;
+        while (len >= 0)
+        {
+            rev = rev + myStr[len];
+            len--;
+        }
+        Console.WriteLine("Reversed String is {0}", rev);
+        Console.ReadLine();
+    }
+}
