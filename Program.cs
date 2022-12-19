a1ab2f2880df3ae98f994811e639c9aa58d965ff
Aniketc72 <69510896+Aniketc72@users.noreply.github.com>
2022-12-19 07:34:12 +0530

    3

--- Program.cs
+++ Program.cs
@@ -1,13 +1,26 @@
-﻿class program
+﻿class Class1
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Number of command line arguements:" + args.Length);
-        Console.WriteLine("command line arguement:\t");
-        for(int i = 0; i < args.Length; i++)
+        Stack s = new Stack();
+        s.Push(1);
+        s.Push(2);
+        s.Push(3);
+        s.Push(4);
+        Console.WriteLine("Original Stack: ");
+        foreach (int i in s)
         {
-            Console.Write(args[i] + "\t");
+            Console.WriteLine(i);
+        }
+        s.Pop();
+        s.Pop();
+        s.Pop();
+        Console.WriteLine("Stack after Pop() operation: ");
+        foreach (int i in s)
+        {
+            Console.WriteLine(i);
         }
         Console.ReadLine();
     }
 }
+ 
