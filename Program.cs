56bdcc72ab5305e25ff27427ddaf177d8e6b8cfc
Aniketc72 <69510896+Aniketc72@users.noreply.github.com>
2022-12-19 07:41:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿class Class3
+{
+    static void Main(string[] args)
+    {
+        string[] cars = new string[4];
+        for (int i = 0; i < cars.Length; i++)
+        {
+            Console.Write("\tElement[" + i + "]: ");
+            cars[i] = Console.ReadLine();
+        }
+        for (int i = 0; i < cars.Length; i++)
+        {
+            Console.WriteLine(cars[i]);
+        }
+        Console.ReadLine();
+    }
+}
