cab20544b4a99bc49846ddf307a84c9bedccb27e
Aniketc72 <69510896+Aniketc72@users.noreply.github.com>
2022-12-19 07:37:00 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿class Class2
+{
+    static void Main(string[] args)
+    {
+        int time = Convert.ToInt32(Console.ReadLine());
+        string result = (time < 18) ? "Good day." : "Good evening.";
+        Console.WriteLine(result);
+        Console.ReadLine();
+    }
+}
